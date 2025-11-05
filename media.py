dcfbcf91c27adf2a9efbd7b52e417eb6de85c4f6
Silas <silashenrique.f@hotmail.com>
2025-11-05 10:27:38 -0300

    início do código de média

---
+++ media.py
@@ -0,0 +1,8 @@
+nome=input("Qual o seu nome: ")
+n1=float(input("diga sua primeira nota: "))
+n2=float(input("diga sua segunda nota: "))
+
+media=(n1 + n2)/2
+
+print(f"aluno {nome} e sua média é {media}!")
+
